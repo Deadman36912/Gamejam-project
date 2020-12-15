f067251d85b5e3f6cb741e1bb81c06a772ef8b84
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2020-12-15 14:24:48 +0100

    Rajouté les commentaires sur le code de LancerObjet

--- Assets/HQ_ResidentialHouse/Scripts/LancerObjet.cs
+++ Assets/HQ_ResidentialHouse/Scripts/LancerObjet.cs
@@ -24,15 +24,22 @@ public class LancerObjet : MonoBehaviour
                 {
                     Transform objectHit = hit.transform;
                     //objectHit.gameObject.SetActive(false);
+                    //Ceci indique le nom de l'objet selectionné
                     Debug.Log(objectHit);
+                    //On enleve le tag Object car sinon cette fonction pourrait se re-appliquer surl'objet selectionné
+                    // /!\ Je pense que l'on pourrait l'enlever
                     objectHit.tag = "Untagged";
+                    //On déplace l'objet en tant que Child de GameObject
                     objectHit.SetParent(Parent1, true);
+                    //On recupére le Rigidbody et on désactive la gravitée et on active isKinematic pour qu'il ne soit plus sujet a la gravité
                     Rigidbody RB = objectHit.GetComponent<Rigidbody>();
                     RB.useGravity = false;
                     RB.isKinematic = true;
+                    //On désactive le collider de l'objet sélectionné pour qu'il ne puisse plus intéragir avec notre personnage
                     Collider col = objectHit.GetComponentInChildren<Collider>();
                     col.enabled = false;
 
+                    //On récupere la position du Parent et on utilise c'est coordonnées pour déplacer l'objet sélectionné
                     //Vector3 position = Parent1.position;
                     Quaternion rotation = Joueur.rotation;
                     objectHit.rotation = rotation;
@@ -53,10 +60,12 @@ public class LancerObjet : MonoBehaviour
         {
             if((Parent1.childCount <= 0))
             {
+                //Cette fonction permet au joueur de prendre un objet
                 SelectObject();
             }
             else
             {
+                //Cette fonction permet au joueur de lancer un objet qui se trouve dans leur mains
                 TrowObject();
             }
         }
