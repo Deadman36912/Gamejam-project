42efca2cfdca033d4dfd9f685239fe91bae59662
LAPTOP-QJT3MD02\Joris <joris.serpollier@lecole-ldlc.com>
2020-12-15 11:33:15 +0100

    Ajout de la fonction Détection et destruction d'objet

---
+++ Assets/HQ_ResidentialHouse/Scripts/LancerObjet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LancerObjet : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Object"))
+                {
+                Transform objectHit = hit.transform;
+                objectHit.gameObject.SetActive(false);
+                Debug.Log(objectHit);
+                 }
+            }
+    }
+}     
