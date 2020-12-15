019d4bf25b8edaaedba574717d7c75105dc63daa
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2020-12-15 13:14:47 +0100

    On peut cliquer sur les objets

--- Assets/HQ_ResidentialHouse/Scripts/LancerObjet.cs
+++ Assets/HQ_ResidentialHouse/Scripts/LancerObjet.cs
@@ -5,23 +5,60 @@ using UnityEngine;
 public class LancerObjet : MonoBehaviour
 {
     public Camera camera;
+    public Transform Parent1;
+    public Transform Parent2;
+    public Transform Joueur;
     // Start is called before the first frame update
     void Start()
     {
     }
-        // Update is called once per frame
-    void Update()
+    void SelectObject()
     {
         RaycastHit hit;
         Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.CompareTag("Object"))
             {
-                if (hit.transform.CompareTag("Object"))
+                if(Parent1.childCount <= 0)
                 {
-                Transform objectHit = hit.transform;
-                objectHit.gameObject.SetActive(false);
-                Debug.Log(objectHit);
-                 }
+                    Transform objectHit = hit.transform;
+                    //objectHit.gameObject.SetActive(false);
+                    Debug.Log(objectHit);
+                    objectHit.tag = "Untagged";
+                    objectHit.SetParent(Parent1, true);
+                    Rigidbody RB = objectHit.GetComponent<Rigidbody>();
+                    RB.useGravity = false;
+                    RB.isKinematic = true;
+                    Collider col = objectHit.GetComponentInChildren<Collider>();
+                    col.enabled = false;
+
+                    //Vector3 position = Parent1.position;
+                    Quaternion rotation = Joueur.rotation;
+                    objectHit.rotation = rotation;
+                    objectHit.localPosition = Vector3.zero;
+                    //objectHit.SetPositionAndRotation(position, rotation);
+                }
             }
+        }
+    }
+    void TrowObject()
+    {
+
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if((Parent1.childCount <= 0))
+            {
+                SelectObject();
+            }
+            else
+            {
+                TrowObject();
+            }
+        }
     }
 }     
