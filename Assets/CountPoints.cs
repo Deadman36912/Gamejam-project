7732ff287e091c0a81b48521fb784b804b83c739
LAPTOP-QJT3MD02\Joris <joris.serpollier@lecole-ldlc.com>
2020-12-18 12:25:58 +0100

    Calcul de points

---
+++ Assets/CountPoints.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountPoints : MonoBehaviour
+{
+    int points;
+    public Text textePoints;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        points += 5;
+        textePoints.text = points.ToString();
+    }
+
+}
